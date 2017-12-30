eaf91f1704bbdcc2ef9c8044f8ee38e7754431ac
Ali <mythologyali@gmail.com>
2017-12-30 02:11:35 -0800

    .

--- Server/app.js
+++ Server/app.js
@@ -23,7 +23,7 @@ class Server {
     // useless object
     constructor(){
         app.set('trust proxy', true);
-        app.use(express.static(__dirname + '../public'));
+        app.use(express.static(path.join(__dirname, '../public')));
         this.dir = path.join(__dirname, '../public/');
     }
 }
